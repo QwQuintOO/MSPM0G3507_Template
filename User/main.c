8742fc83070815e2374f3a97d0fe427c66ec7c79
Qwerty <3684961430@qq.com>
2026-06-20 17:15:43 +0800

    wip

--- User/main.c
+++ User/main.c
@@ -1,8 +1,14 @@
 #include "main.h"
 
 
-#include "mspm0g350x.h"
+void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName)
+{
+    (void)xTask;
+    (void)pcTaskName;
 
+    taskDISABLE_INTERRUPTS();
+    for( ;; );
+}
 
 int main (void) 
 {
--- User/main.h
+++ User/main.h
@@ -1,8 +1,10 @@
 #ifndef     __MAIN_H
 #define     __MAIN_H
 
-
-
+#include "mspm0g350x.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
 
 #endif  /* __MAIN_H */
 
